af810b40a8e7830d0643b9992c0881fda94e75b2
Vutshi <d.vutshi@gmail.com>
2026-05-06 23:04:12 +0200

    Revert "monster: avoid multiply in field row lookup"
    This reverts commit 4e0bf4a5b5550bdaae23aaf8b8319807bd92cd4d.

--- src/monster.c
+++ src/monster.c
@@ -503,8 +503,7 @@ void mondie(Sint4 mon)
 }
 
 #ifdef DIGGER_ELKS
-static const Sint4 monfieldrow[MHEIGHT]={0,15,30,45,60,75,90,105,120,135};
-#define MONFIELDIDX(h,v) (monfieldrow[(v)]+(h))
+#define MONFIELDIDX(h,v) (((v)<<4)-(v)+(h))
 
 bool fieldclear(Sint4 dir,Sint4 x,Sint4 y)
 {
